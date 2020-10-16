9bd6582596c9e4b759d71aa4eb2786fd21c3af3c
gabrielmendes98 <gabrielmssantiago@gmail.com>
2020-10-16 11:19:40 -0300

    refatoracao de pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import {FormsModule} from '@angular/forms'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
-import {ROUTES} from './app.routes'
+import { ROUTES } from "./app.routes";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { RestaurantsComponent } from "./pages/restaurants/restaurants.component";
+import { RestaurantComponent } from "./pages/restaurants/restaurant/restaurant.component";
+import { RestaurantsService } from "./pages/restaurants/restaurants.service";
+import { RestaurantDetailComponent } from "./pages/restaurant-detail/restaurant-detail.component";
+import { MenuComponent } from "./pages/restaurant-detail/menu/menu.component";
+import { ShoppingCartComponent } from "./pages/restaurant-detail/shopping-cart/shopping-cart.component";
+import { MenuItemComponent } from "./pages/restaurant-detail/menu-item/menu-item.component";
+import { ReviewsComponent } from "./pages/restaurant-detail/reviews/reviews.component";
 
-import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component'
+import { ShoppingCartService } from "./pages/restaurant-detail/shopping-cart/shopping-cart.service";
+import { OrderComponent } from "./pages/order/order.component";
+import { InputComponent } from "./shared/input/input.component";
+import { RadioComponent } from "./shared/radio/radio.component";
+import { OrderItemsComponent } from "./pages/order/order-items/order-items.component";
 
-import {OrderService} from "./order/order.service";
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+import { OrderService } from "./pages/order/order.service";
+import { DeliveryCostsComponent } from "./pages/order/delivery-costs/delivery-costs.component";
 
 @NgModule({
   declarations: [
@@ -45,15 +45,20 @@ import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.co
     InputComponent,
     RadioComponent,
     OrderItemsComponent,
-    DeliveryCostsComponent
-   ],
+    DeliveryCostsComponent,
+  ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
-  bootstrap: [AppComponent]
+  providers: [
+    RestaurantsService,
+    ShoppingCartService,
+    OrderService,
+    { provide: LOCALE_ID, useValue: "pt-BR" },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,25 @@
-import {Routes} from '@angular/router'
+import { Routes } from "@angular/router";
 
-import {HomeComponent} from './home/home.component'
-import {RestaurantsComponent} from './restaurants/restaurants.component'
-import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component'
-import {AboutComponent} from './about/about.component'
-import {MenuComponent} from './restaurant-detail/menu/menu.component'
-import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component'
-import {OrderComponent} from './order/order.component'
+import { HomeComponent } from "./pages/home/home.component";
+import { RestaurantsComponent } from "./pages/restaurants/restaurants.component";
+import { RestaurantDetailComponent } from "./pages/restaurant-detail/restaurant-detail.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { MenuComponent } from "./pages/restaurant-detail/menu/menu.component";
+import { ReviewsComponent } from "./pages/restaurant-detail/reviews/reviews.component";
+import { OrderComponent } from "./pages/order/order.component";
 
 export const ROUTES: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'restaurants', component: RestaurantsComponent},
-  {path: 'restaurants/:id', component: RestaurantDetailComponent,
+  { path: "", component: HomeComponent },
+  { path: "restaurants", component: RestaurantsComponent },
+  {
+    path: "restaurants/:id",
+    component: RestaurantDetailComponent,
     children: [
-      {path: '', redirectTo: 'menu', pathMatch: 'full'},
-      {path: 'menu', component: MenuComponent},
-      {path: 'reviews', component: ReviewsComponent}
-    ]},
-  {path: 'order', component: OrderComponent},
-  {path: 'about', component: AboutComponent}
-]
+      { path: "", redirectTo: "menu", pathMatch: "full" },
+      { path: "menu", component: MenuComponent },
+      { path: "reviews", component: ReviewsComponent },
+    ],
+  },
+  { path: "order", component: OrderComponent },
+  { path: "about", component: AboutComponent },
+];

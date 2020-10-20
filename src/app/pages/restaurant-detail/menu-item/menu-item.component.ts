93ff3f97698372dd3635081a8d343357a626d8f7
gabrielmendes98 <gabrielmssantiago@gmail.com>
2020-10-20 10:57:25 -0300

    animacao dos restaurantes e dos itens de menu

--- src/app/pages/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/pages/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
-import {MenuItem} from "./menu-item.model"
+import { MenuItem } from './menu-item.model';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state(
+        'ready',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('void => ready', [
+        style({
+          opacity: 0,
+          transform: 'translate(-20px)',
+        }),
+        animate('500ms 0s ease-in-out'),
+      ]),
+    ]),
+  ],
 })
 export class MenuItemComponent implements OnInit {
+  @Input() menuItem: MenuItem;
+  @Output() add = new EventEmitter();
 
-  @Input() menuItem: MenuItem
-  @Output() add = new EventEmitter()
+  menuItemState = 'ready';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  emitAddEvent() {
+    this.add.emit(this.menuItem);
   }
-
-  emitAddEvent(){
-    this.add.emit(this.menuItem)
-  }
-
 }

6256833316b65f4af1063ad3f12987fe1472c162
gabrielmendes98 <gabrielmssantiago@gmail.com>
2020-10-22 09:36:29 -0300

    error handler modificado pra nova versao e refatoracao backend

--- src/app/pages/order/order.service.ts
+++ src/app/pages/order/order.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
-import { Http, Headers, RequestOptions } from "@angular/http";
-import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
-import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
-import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
-import { Order, OrderItem } from "./order.model";
+import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
 
-import { MEAT_API } from "../../app.api";
+import { MEAT_API } from '../../app.api';
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService, private http: Http) {}
+  constructor(private cartService: ShoppingCartService, private http: HttpClient) {}
 
   itemsValue(): number {
     return this.cartService.total();
@@ -39,15 +39,6 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    return this.http
-      .post(
-        `${MEAT_API}/orders`,
-        JSON.stringify(order),
-        new RequestOptions({ headers: headers })
-      )
-      .map((response) => response.json())
-      .map((order) => order.id);
+    return this.http.post<Order>(`${MEAT_API}/orders`, order).map((order) => order.id);
   }
 }
